dc788af7409cdabc06ab7b8997f69550acb4a4c9
SongQian <onlylove1172559463@vip.qq.com>
2021-06-08 22:51:48 +0800

    test

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Developer    :   SongQian
+ * Time         :   2020-06-01
+ * eMail        :   onlylove1172559463@vip.qq.com
+ * Description  :   Magic Cube Project Start
+ */
+
+const Run = function() : void {
+    console.log("service running");
+}
+
+export const MagicCube = Run;
