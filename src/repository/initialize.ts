31b8c1f0d86529d5f5f18cc8a73270f4c752fbaf
skysong <skysong@debian-BULLSEYE-live-builder-AMD64>
2022-09-03 21:37:30 +0800

    modify createTableIfNotExists

--- src/repository/initialize.ts
+++ src/repository/initialize.ts
@@ -26,9 +26,9 @@ export default function(configure: any) {
         let repositorys = ResolverModuleFactory.getInstance().GetAnyModels<IRepository>(Symbol.for("magic:table"));
         repositorys.map((repository : IRepository) => {
             repository.dbContext = dbContext;
-            const tableName = Reflect.getMetadata(Symbol.for("magic:table"), repository);
-            const tableViewExpression = Reflect.getMetadata(Symbol.for("magic:tableViewExpression"), repository);
-            const isDrop = Reflect.getMetadata(Symbol.for("magic:dropTableIfExists"), repository);
+            const tableName = Reflect.getMetadata(Symbol.for("magic:table"), repository.constructor);
+            const tableViewExpression = Reflect.getMetadata(Symbol.for("magic:tableViewExpression"), repository.constructor);
+            const isDrop = Reflect.getMetadata(Symbol.for("magic:dropTableIfExists"), repository.constructor);
             let synchronousPipe : PromiseLike<boolean> = Promise.resolve(false);
             
             if (isDrop) {
@@ -47,7 +47,7 @@ export default function(configure: any) {
                 return yes ? Promise.resolve(!yes) : new Promise((resolve, reject) => {
                     const isBeforeTableInit = repository.$beforeTableInitialize ? (repository.$beforeTableInitialize() || true) : false;
                     if (!Boolean(tableViewExpression)) {
-                        dbContext.schema.createTableIfNotExists(tableName, (table) => {
+                        const createFn = (table) => {
                             const tableEngine = Reflect.getMetadata(Symbol.for("magic:tableEngine"), repository)
                             const tableCharset = Reflect.getMetadata(Symbol.for("magic:tableCharset"), repository)
                             const tableCollate = Reflect.getMetadata(Symbol.for("magic:tableCollate"), repository)
@@ -100,7 +100,64 @@ export default function(configure: any) {
                                 })
                             }
                             isBeforeTableInit && isPromise(isBeforeTableInit) ?  (<Promise<void>>isBeforeTableInit).then(initializeTablePropsDone).then(resolve, reject) : Promise.resolve(initializeTablePropsDone()).then(resolve, reject)
-                        })
+                        }
+                        dbContext.schema.hasTable(tableName).then(yes => yes ? resolve(false) : dbContext.schema.createTable(tableName, createFn));
+
+                        // dbContext.schema.createTableIfNotExists(tableName, (table) => {
+                        //     const tableEngine = Reflect.getMetadata(Symbol.for("magic:tableEngine"), repository)
+                        //     const tableCharset = Reflect.getMetadata(Symbol.for("magic:tableCharset"), repository)
+                        //     const tableCollate = Reflect.getMetadata(Symbol.for("magic:tableCollate"), repository)
+                        //     table.charset(tableCharset);
+                        //     table.engine(tableEngine);
+                        //     table.collate(tableCollate);
+                        //     const initializeTablePropsDone = () : Promise<boolean> => {
+                        //         const initializePropsDone = (field : string) : Promise<Boolean> => {
+                        //             const columnName = Reflect.getMetadata(Symbol.for("magic:tableColumnName"), repository, field);
+                        //             if (columnName) {
+                        //                 const columnType = Reflect.getMetadata(Symbol.for("magic:tableColumnType"), repository, field);
+                        //                 const columnComment = Reflect.getMetadata(Symbol.for("magic:tableColumnComment"), repository, field);
+                        //                 const columnOptions = Reflect.getMetadata(Symbol.for("magic:tableColumnOptions"), repository, field);
+                        //                 const columnDefaultValue = Reflect.getMetadata(Symbol.for("magic:tableColumnDefaultValue"), repository, field);
+                        //                 const columnDefaultOptions = Reflect.getMetadata(Symbol.for("magic:tableColumnDefaultOptions"), repository, field);
+                        //                 const columnNullable = Reflect.getMetadata(Symbol.for("magic:tableColumnNullable"), repository, field);
+                        //                 const columnNotNullable = Reflect.getMetadata(Symbol.for("magic:tableColumnNotNullable"), repository, field);
+                        //                 const columnIndex = Reflect.getMetadata(Symbol.for("magic:tableIndexName"), repository, field);
+                        //                 const columnIndexOptions : TableIndex = <TableIndex>Reflect.getMetadata(Symbol.for("magic:tableIndexOptions"), repository, field);
+                        //                 const columnUnique = Reflect.getMetadata(Symbol.for("magic:tableUniqueName"), repository, field);
+                        //                 const columnUniqueOptions : UniqueIndex = <UniqueIndex>Reflect.getMetadata(Symbol.for("magic:tableUniqueOptions"), repository, field);
+                        //                 const columnPrimary = Reflect.getMetadata(Symbol.for("magic:tablePrimaryKey"), repository, field);
+                        //                 const columnPrimaryOptions : PrimaryKey = <PrimaryKey>Reflect.getMetadata(Symbol.for("magic:tablePrimaryOptions"), repository, field);
+                        //                 const columnIncrements = Reflect.getMetadata(Symbol.for("magic:tableIncrementsColumn"), repository, field);
+                        //                 const columnIncrementsOptions = Reflect.getMetadata(Symbol.for("magic:tableIncrementsOptions"), repository, field);
+                        //                 const columnForeignKey = Reflect.getMetadata(Symbol.for("magic:tableForeignKey"), repository, field);
+                        //                 const columnForeignOptions = Reflect.getMetadata(Symbol.for("magic:tableForeignOptions"), repository, field);
+                        //                 const columnFactory = [ table.integer, table.bigInteger, table.text, table.string, table.float, table.double, table.decimal, table.boolean, table.date, table.dateTime, table.time, table.timestamp, table.timestamps, table.binary, table.enum, table.json, table.jsonb, table.uuid, table.geometry, table.geography, table.point ];
+                        //                 let columnBuilder = <KnexSchema.ColumnBuilder>(<any>columnFactory[columnType]).apply(table, [columnName, ...columnOptions]);
+                        //                 if (clientName === "MYSQL") {
+                        //                     columnBuilder = columnDefaultValue && columnBuilder.defaultTo(columnDefaultValue, columnDefaultOptions) || columnBuilder;
+                        //                 }
+                        //                 columnBuilder = columnComment && columnBuilder.comment(columnComment) || columnBuilder;
+                        //                 columnBuilder = !columnNotNullable && columnNullable && columnBuilder.nullable() || columnBuilder;
+                        //                 columnIndex && table.index(columnName, columnIndex, columnIndexOptions);
+                        //                 columnUnique && table.unique(columnName, columnUniqueOptions);
+                        //                 columnPrimary && table.primary(columnName, columnPrimaryOptions);
+                        //                 columnIncrements && table.increments(columnName, columnIncrementsOptions);
+                        //                 columnForeignKey && table.foreign(field, columnForeignKey).references(`${columnForeignOptions.table}.${columnForeignOptions.foreignColumn}`).onDelete(columnForeignOptions.onDelete || "CASCADE").onUpdate(columnForeignOptions.onUpdate || "CASCADE");
+                        //                 const properitesState : ColumnPropertiesState = { type: columnType, comment: Boolean(columnComment), default: Boolean(columnDefaultValue), nullable: !columnNotNullable && columnNullable, index: Boolean(columnIndex), unique: Boolean(columnUnique), primary: Boolean(columnPrimary), increments : Boolean(columnIncrements), foreign: Boolean(columnForeignKey) };
+                        //                 const updateColumnPropsPromise = repository.$updateTableColumnProps && repository.$updateTableColumnProps(table, columnName, properitesState);
+                        //                 return isPromise(updateColumnPropsPromise) ? (<Promise<void>>updateColumnPropsPromise).then(() => true) : Promise.resolve(true);
+                        //             }
+                        //             return Promise.resolve(false)
+                        //         }
+                        //         const updateColumnsDone = () => Object.keys(repository).map(initializePropsDone);
+                        //         return Promise.all(updateColumnsDone()).then(() => {
+                        //             const isAfterTableInit  =  repository.$afterTableInitialized && repository.$afterTableInitialized(table) || false;
+                        //             return isPromise(isAfterTableInit) ? (<Promise<void>>isAfterTableInit).then(() => true) : Promise.resolve(true);
+                        //         })
+                        //     }
+                        //     isBeforeTableInit && isPromise(isBeforeTableInit) ?  (<Promise<void>>isBeforeTableInit).then(initializeTablePropsDone).then(resolve, reject) : Promise.resolve(initializeTablePropsDone()).then(resolve, reject)
+                        // })
+
                     }
 
                     if (Boolean(tableViewExpression)) {
